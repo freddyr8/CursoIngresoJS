bb2543e56a5ef68df1dc4b1fb1206104b2fc2c63
Your Name <rojas.freddyely@gmail.com>
2020-02-07 17:26:05 -0300

    Ferrete construccion y facturacion

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,74 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    var largoTerrenoIngresado;
+    var anchoTerrenoIngresado;
+    var largoTerrenoParseado;
+    var anchoTerrenoParseado;
+    var operacion;
+    var resultado;
+   
+    //Obtener datos por ID
+    largoTerrenoIngresado = document.getElementById("Largo").value;
+    anchoTerrenoIngresado = document.getElementById("Ancho").value;
+
+    //Parseo de datos
+    largoTerrenoParseado = largoTerrenoIngresado;
+    anchoTerrenoParseado = anchoTerrenoIngresado;
+
+    //Operacion
+    operacion = (largoTerrenoParseado)*2 + (anchoTerrenoParseado)*2;
+    resultado = operacion*3;
+
+    //Resultado
+    alert("La cantidad a comprar serian " + resultado + "mts de alambre ");
+
 
 }
 function Circulo () 
 {
-	
+    var radioIngresado;
+    var radioParseado;
+    var operacion;
+    var resultado;
+
+    //Obtener datos por ID
+    radioIngresado = document.getElementById("Radio").value;
+
+    //Parseo de datos
+    radioParseado = radioIngresado;
+
+    //Operacion
+    operacion = radioParseado*2;
+    resultado = operacion*Math.PI;
+
+    //Resultado
+    alert("La cantidad de mts de alambre a comprar serian: " + resultado);
 }
 function Materiales () 
 {
+    var largoTerrenoIngresado;
+    var anchoTerrenoIngresado;
+    var largoTerrenoParseado;
+    var anchoTerrenoParseado;
+    var operacion;
+    var resultadoCem;
+    var resultadoCal
+   
+    //Obtener datos por ID
+    largoTerrenoIngresado = document.getElementById("Largo").value;
+    anchoTerrenoIngresado = document.getElementById("Ancho").value;
+
+    //Parseo de datos
+    largoTerrenoParseado = largoTerrenoIngresado;
+    anchoTerrenoParseado = anchoTerrenoIngresado;
+
+    //Operacion
+    operacion = (largoTerrenoParseado)*2 + (anchoTerrenoParseado)*2;
+    resultadoCem = operacion*2;
+    resultadoCal = operacion*3;
+
+    //Resultado
+    alert("Se necesitan " + resultadoCem + " bolsas de cemento y " + resultadoCal + " bolsas de cal");
 	
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,88 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var numeroUnoIngresado;
+    var numeroDosIngresado;
+    var numeroTresIngresado;
+    var numeroUnoParseado;
+    var numeroDosParseado;
+    var numeroTresParseado;
+    var resultadoUno;
+    var resultadoDos;
+    var resultadoTres;
+
+    //Obtener datos por ID
+    numeroUnoIngresado = document.getElementById("PrecioUno").value;
+    numeroDosIngresado = document.getElementById("PrecioDos").value;
+    numeroTresIngresado = document.getElementById("PrecioTres").value;
+    
+    //Parseo de datos
+    numeroUnoParseado = parseInt(numeroUnoIngresado);
+    numeroDosParseado = parseInt(numeroDosIngresado);
+    numeroTresParseado = parseInt(numeroTresIngresado);
+    
+    //Operacion
+    resultadoUno = numeroUnoParseado + numeroDosParseado + numeroTresParseado;
+    
+    //Resultado
+    alert(resultadoUno);
 }
 function Promedio () 
 {
-	
+    var numeroUnoIngresado;
+    var numeroDosIngresado;
+    var numeroTresIngresado;
+    var numeroUnoParseado;
+    var numeroDosParseado;
+    var numeroTresParseado;
+    var resultadoUno;
+    var resultadoDos;
+
+    //Obtener datos por ID
+    numeroUnoIngresado = document.getElementById("PrecioUno").value;
+    numeroDosIngresado = document.getElementById("PrecioDos").value;
+    numeroTresIngresado = document.getElementById("PrecioTres").value;
+    
+    //Parseo de datos
+    numeroUnoParseado = parseInt(numeroUnoIngresado);
+    numeroDosParseado = parseInt(numeroDosIngresado);
+    numeroTresParseado = parseInt(numeroTresIngresado);
+    
+    //Operacion
+    resultadoUno = numeroUnoParseado + numeroDosParseado + numeroTresParseado;
+    resultadoDos = resultadoUno/3;
+    
+    //Resultado
+    alert(resultadoDos);
 }
 function PrecioFinal () 
 {
+    var numeroUnoIngresado;
+    var numeroDosIngresado;
+    var numeroTresIngresado;
+    var numeroUnoParseado;
+    var numeroDosParseado;
+    var numeroTresParseado;
+    var resultadoUno;
+    var resultadoDos;
+    var resultadoTres;
+
+    //Obtener datos por ID
+    numeroUnoIngresado = document.getElementById("PrecioUno").value;
+    numeroDosIngresado = document.getElementById("PrecioDos").value;
+    numeroTresIngresado = document.getElementById("PrecioTres").value;
+    
+    //Parseo de datos
+    numeroUnoParseado = parseInt(numeroUnoIngresado);
+    numeroDosParseado = parseInt(numeroDosIngresado);
+    numeroTresParseado = parseInt(numeroTresIngresado);
+    
+    //Operacion
+    resultadoUno = numeroUnoParseado + numeroDosParseado + numeroTresParseado;
+    resultadoDos = resultadoUno*0.21;
+    resultadoTres = resultadoUno + resultadoDos;
+    
+    //Resultado
+    alert(resultadoTres);
 	
 }
